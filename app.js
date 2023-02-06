c549cbe5090c37f75ce09aeca99c47fa5d46757e
unkitsingh0 <ankitsingh.302.as@gmail.com>
2023-02-06 21:59:44 +0530

    'a'

---
+++ app.js
@@ -0,0 +1,20 @@
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('chal bahg');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+// });
+
+let numb1 = 12;
+console.log(numb1);
+
+numb1+=3;
+console.log(numb1)
