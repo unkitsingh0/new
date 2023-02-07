a76e57054d604450d0d0d44e1bf745639ea806ed
unkitsingh0 <ankitsingh.302.as@gmail.com>
2023-02-07 19:05:41 +0530

    adding some more code whiche i have leared on 7 feb 2023

--- app.js
+++ app.js
@@ -13,8 +13,31 @@
 //   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
 // });
 
-let numb1 = 12;
-console.log(numb1);
+// let numb1 = 12;
+// let numb2 = 12
+// console.log(numb1);
 
-numb1+=3;
-console.log(numb1)
+// numb1+=3;
+// console.log(numb1)
+// console.log(numb2 + ++numb1); // prefix
+// console.log(numb2 + numb1++); // postfix
+// console.log(numb1)
+
+// console.log(numb1 >= numb2)
+// console.log(numb1 == numb2)
+// console.log(numb1 === numb2)
+
+
+// let b = numb1 != numb2;
+// console.log(b)
+// numb1 === numb2 ? console.log("done") : console.log("not done")
+
+
+let age = 10;
+let citizen = "india";
+// && is and opratore it will check both the condition and then give response ad true 
+// let canvote = age >= 18 && citizen == "indian" ? "can vote" : "can not vote";
+// console.log(canvote)
+// || is or opratore it will check both the condition and if any one condition is true it will reture the value of ture or false 
+let canvote = age >= 18 || citizen == 'indian' ? "can vote" : "can not vote "
+console.log(canvote)
